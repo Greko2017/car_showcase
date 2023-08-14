efd8ef0c9857c659269e48207b822ac8a7b6a498
Gregory Goufan <gregory.goufan@gmail.com>
2023-08-14 16:42:50 +0100

    first commit

---
+++ types/index.ts
@@ -0,0 +1,79 @@
+import { MouseEventHandler } from "react";
+
+export interface OptionProps {
+  title: string;
+  value: string;
+}
+export interface CustomFilterProps {
+    title: string;
+    options: OptionProps[];
+}
+
+export interface CarProps {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
+  }
+  
+  export interface FilterProps {
+    manufacturer?: string;
+    year?: number;
+    model?: string;
+    limit?: number;
+    fuel?: string;
+    pageNumber?: number;
+  }
+  
+  export interface HomeProps {
+    searchParams: FilterProps;
+  }
+  
+  export interface CarCardProps {
+    model: string;
+    make: string;
+    mpg: number;
+    transmission: string;
+    year: number;
+    drive: string;
+    cityMPG: number;
+  }
+  
+  export interface CustomButtonProps {
+    isDisabled?: boolean;
+    btnType?: "button" | "submit";
+    containerStyles?: string;
+    textStyles?: string;
+    title: string;
+    rightIcon?: string;
+    handleClick?: MouseEventHandler<HTMLButtonElement>;
+  }
+  
+  export interface OptionProps {
+    title: string;
+    value: string;
+  }
+  
+  export interface CustomFilterProps {
+    title: string;
+    options: OptionProps[];
+  }
+  
+  export interface ShowMoreProps {
+    pageNumber: number;
+    isNext: boolean;
+    // setLimit: 
+  }
+  
+  export interface SearchManuFacturerProps {
+    manufacturer: string;
+    setManuFacturer: (manufacturer: string) => void;
+  }
